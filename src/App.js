d9a2f7d6aa8fd17b99fba817be56ee99a66be0ee
randiayeshanikossinna <randiayeshani@gmail.com>
2021-06-20 03:27:48 -0700

    navigation bar is created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
+import {BrowserRouter as Router,Switch,Route} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <h1>welcome</h1>
-    </div>
+   <>
+      <Router>
+        <Navbar/>
+        <Switch>
+          <Route path='/' exact/>
+        </Switch>
+      </Router>
+         
+   </>
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,21 @@
+import React,{useState} from 'react'
+import {Link} from 'react-router-dom';
+
+function Navbar() {
+    return (
+        <>
+          <nav className="navbar">
+            <div className="navbar-container">
+                <Link to="/" className="navbar-logo">TRAVEL
+                <i className="fab fa-typo3"></i>
+                </Link>
+              
+            </div>
+          </nav>
+        
+
+        </>
+    )
+}
+
+export default Navbar
