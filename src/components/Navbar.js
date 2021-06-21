70070211c2b788b7d760238e64ee5fa209af663c
randiayeshanikossinna <randiayeshani@gmail.com>
2021-06-21 12:16:47 -0700

    sign up button was added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,23 @@
 import React,{useState} from 'react'
 import {Link} from 'react-router-dom';
 import { Button } from './Button';
-
+import './navbar.css';
 function Navbar() {
     const [click, setClick] = useState(false);
     const [button, setButton] = useState(true);
 
     const handleClick = () => setClick(!click);
     const closeMobileMenu = () => setClick(false);
+
+    const showButton = () => {
+      if (window.innerWidth <= 960) {
+        setButton(false);
+      } else {
+        setButton(true);
+      }
+    };
+  
+    window.addEventListener('resize', showButton);
     return (
         <>
           <nav className="navbar">
