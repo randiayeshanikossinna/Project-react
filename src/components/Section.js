a40d3d6960ba6a0e4f76528255ef995f70e4b544
randiayeshanikossinna <randiayeshani@gmail.com>
2021-06-21 21:33:38 -0700

    Home section is started

---
+++ src/components/Section.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Section() {
+    return (
+        <div className="section-container">
+            <video src="" autoPlay loop muted/>
+        </div>
+    )
+}
+
+export default Section
+
+
